b0e074f44adce4924f1ca6c8b363df815be0d095
sdaviid <sdaviid@gmail.com>
2022-06-17 19:33:46 -0300

    upload remote

--- app/core/uptobox.py
+++ app/core/uptobox.py
@@ -16,3 +16,32 @@ class uptobox(object):
         except Exception as err:
             print(f'uptobox.status exception - {err}')
         return False
+    def upload_remote_server(self):
+        url = self.build_url('/api/upload')
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                if response.json().get('statusCode', -1) == 0:
+                    temp_upload_url = response.json().get('data', {}).get('uploadLink', False)
+                    if temp_upload_url:
+                        return f'https://{temp_upload_url[2:]}'
+        except Exception as err:
+            print(f'uptobox.upload_remote_server exception - {err}')
+        return False
+    def upload_remote(self, link):
+        url = self.upload_remote_server()
+        if url:
+            url = url.replace('upload', 'remote')
+            session = requests.Session()
+            headers = {
+                'content-type': 'application/x-www-form-urlencoded'
+            }
+            payload = {
+                'urls': f'["{link}"]'
+            }
+            with session.post(url, data=payload, headers=headers, stream=True) as resp:
+                for line in resp.iter_lines():
+                    if line:
+                        print(line)
+
+
